b5a333fe390b68a302b875c0551c5d12b84396cc
= <=>
2022-03-16 14:19:44 +0300

    1.0

---
+++ src/Library.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Library.php
+ *
+ * This file is part of Config.
+ *
+ * @author     Muhammet ŞAFAK <info@muhammetsafak.com.tr>
+ * @copyright  Copyright © 2022 Config
+ * @license    http://www.gnu.org/licenses/gpl-3.0.txt  GNU GPL 3.0
+ * @version    1.0
+ * @link       https://www.muhammetsafak.com.tr
+ */
+
+declare(strict_types=1);
+
+namespace InitPHP\Config;
+
+class Library
+{
+
+}
